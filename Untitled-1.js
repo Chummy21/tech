be1ada0ad47c0ebab2815f5e1a9ea97c2868fffd
Chioma Okafor <chiomagerald@gmail.com>
2025-01-24 19:08:47 +0100

    hello

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+//Declare variables named petDog and petCat give them names Rex and Pepper.
+let petDog = "Rex";
+let petCat = "Pepper";
+//console.log the variables
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+//Declare more variables
+let catSound = "purr";
+let dogSound = "woof";
+//console.log text and variables
+console.log(petDog + " says " + dogSound);
+console.log(petCat + " says " + catSound);
+//Reassign the value in catSound to te string "meow"
+catSound = " meow ";
+//Console.log the variable petCat, then the string "now says", ten te variable catSound
+console.log(petCat + " now says " + catSound);
